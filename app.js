65b93ab5e429f3e65849553dc2ecf74cfc6e803d
Zirth66 <zirth66@gmail.com>
2021-05-06 21:18:17 +0200

    Conditional rendering test

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("Hello, vue")
+const app = Vue.createApp({
+    data() {
+        return {
+            showBooks: true,
+            title: "The final empire", 
+            author: "Brandon Sanderson", 
+            age: 45
+        }
+    },
+    methods: {
+        toggleShowBooks() {
+            this.showBooks = !this.showBooks
+        }
+    }
+})
+
+app.mount("#app")
