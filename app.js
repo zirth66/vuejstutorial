ef98eef85649d253879cde9d08ef932282679407
Zirth66 <zirth66@gmail.com>
2021-05-10 18:00:41 +0200

    #3 part one

--- app.js
+++ app.js
@@ -4,12 +4,21 @@ const app = Vue.createApp({
             showBooks: true,
             title: "The final empire", 
             author: "Brandon Sanderson", 
-            age: 45
+            age: 45,
+            x: 0,
+            y: 0
         }
     },
     methods: {
         toggleShowBooks() {
             this.showBooks = !this.showBooks
+        },
+        handleEvent(e) {
+            console.log(e, e.type)
+        }, 
+        handleMousemove(e) {
+            this.x = e.offsetX,
+            this.y = e.offsetY
         }
     }
 })
